a53f524230004b5c2bab3d0475013175c5311a58
Ying Yi <ying.yi@sony.com>
2018-04-24 12:28:22 +0100

    Moved RepoTicketGenerationPass and RepoPruningPass to the beginning of the ModulePassand Changed the GO's ticket digest metadata by adding the hash of its dependent global objects.

--- llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
+++ llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -425,6 +425,9 @@ void PassManagerBuilder::addFunctionSimplificationPasses(
 
 void PassManagerBuilder::populateModulePassManager(
     legacy::PassManagerBase &MPM) {
+  MPM.add(createRepoTicketGenerationPass());
+  MPM.add(createRepoPruningPass());
+
   if (!PGOSampleUse.empty()) {
     MPM.add(createPruneEHPass());
     MPM.add(createSampleProfileLoaderPass(PGOSampleUse));
@@ -441,9 +444,6 @@ void PassManagerBuilder::populateModulePassManager(
       MPM.add(Inliner);
       Inliner = nullptr;
     }
-
-    MPM.add(createRepoTicketGenerationPass());
-    MPM.add(createRepoPruningPass());
 
     // FIXME: The BarrierNoopPass is a HACK! The inliner pass above implicitly
     // creates a CGSCC pass manager, but we don't want to add extensions into
@@ -557,9 +557,6 @@ void PassManagerBuilder::populateModulePassManager(
 
   if (RunPartialInlining)
     MPM.add(createPartialInliningPass());
-
-  MPM.add(createRepoTicketGenerationPass());
-  MPM.add(createRepoPruningPass());
 
   if (OptLevel > 1 && !PrepareForLTO && !PrepareForThinLTO)
     // Remove avail extern fns and globals definitions if we aren't
